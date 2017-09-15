610bf0d95c3c4c08d57291b746b8bfd067d239bc
Chris_Bo <krzysztofbogusz050591@gmail.com>
2017-09-15 15:49:03 +0200

    new

---
+++ 12# - Mutations.js
@@ -0,0 +1,19 @@
+function mutation(arr) {
+
+    var arr1 = arr[1].toLowerCase().split('');
+    var arr2 = arr[0].toLowerCase().split('');
+
+
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            return false;
+        }
+    }
+    return true;
+
+
+
+
+}
+
+mutation(["hello", "hey"]);
