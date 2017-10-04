037458a4480356a22bb05d2cb09efa974248d5d8
Chris_Bo <krzysztofbogusz050591@gmail.com>
2017-10-04 09:21:42 +0200

    Add files via upload

---
+++ Intermediate Algorithm Scripting/9 # - Boo who.js
@@ -0,0 +1,23 @@
+/*
+Boo who
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+*/
+
+//My solution
+
+
+function booWho(bool) {
+
+
+    return bool === Boolean(bool);
+}
+
+booWho(false);
